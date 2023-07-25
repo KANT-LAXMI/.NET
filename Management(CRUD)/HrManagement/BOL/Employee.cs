39d8d650b44163adcf691357fe401e90e6d76b08
Laxmi <kantlaxmi1357@gmail.com>
2023-07-26 01:15:36 +0530

    'MVC'

---
+++ Management(CRUD)/HrManagement/BOL/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BOL
+{
+    public  class Employee
+    {
+
+        [Key]
+        public int EmpId { get; set; } = 0;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public double Salary { get; set; } = 0;
+
+        public Employee()
+        {
+        }
+
+        public Employee(int empId, string firstName, string lastName,string department, string email, string password, double sal)
+        {
+            EmpId = empId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            Salary = sal;
+            Department = department;
+        }
+
+
+    }
+}
