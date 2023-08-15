7b4f87bf29d9de6a39fb88f007bb8272a620885e
Laxmi <kantlaxmi1357@gmail.com>
2023-08-15 23:26:00 +0530

    'rest'

---
+++ Student/Student/Data/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Student.Data.Entities
+{
+    public class Student
+    {
+        public int? id { get; set; } 
+        public string? name { get; set; }
+        public int? age { get; set; }
+            
+        public string? email { get; set; }   
+     
+
+          }
+}
---
+++ Student/Student/Data/MyWorldDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Student.Data.Entities;
+namespace Student.Data;
+
+public class MyWorldDbContext:DbContext
+{
+    public MyWorldDbContext(DbContextOptions<MyWorldDbContext> context) : base(context)
+    { }
+    public DbSet<Student> Students { get; set; }
+}
+
